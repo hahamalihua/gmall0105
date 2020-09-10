5c91db2868591977038dfe4410568317d5bb6dfa
malihua <625426865@qq.com>
2020-09-10 12:19:39 +0800

    day1-1

--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.atguigu.gmall.user.service.impl;
 
 import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.atguigu.gmall.user.mapper.UserMapper;
 import com.atguigu.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -14,9 +17,21 @@ public class UserServiceImpl implements UserService{
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
     @Override
     public List<UmsMember> getAllUser() {
         List<UmsMember> umsMemberList=userMapper.selectAll();
         return umsMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        UmsMemberReceiveAddress umsMemberReceiveAddress=new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+        return umsMemberReceiveAddresses;
     }
 }
